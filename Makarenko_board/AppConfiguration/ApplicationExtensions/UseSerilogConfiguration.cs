8e9dac74df3f96466a502953f55267aa135a3b49
speakland <speakland@bk.ru>
2022-12-13 15:12:41 +0300

    Add lab 2

---
+++ Makarenko_board/AppConfiguration/ApplicationExtensions/UseSerilogConfiguration.cs
@@ -0,0 +1,16 @@
+using Serilog;
+
+namespace Board.WebAPI.AppConfiguration.ApplicationExtensions
+{
+    public static partial class AppExtensions
+    {
+        /// <summary>
+        /// Use serilog configuration
+        /// </summary>
+        /// <param name="app"></param>
+        public static void UseSerilogConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSerilogRequestLogging();
+        }
+    }
+}
